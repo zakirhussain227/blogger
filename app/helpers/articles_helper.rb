c57472d0679df1268b29d135ec9dd511f28cc65d
Zakir <zakirawkum@gmail.com>
2017-07-23 11:34:01 +0500

    added a few gems

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module ArticlesHelper
     params.require(:article).permit(:title, :body)
   end
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 
 end
